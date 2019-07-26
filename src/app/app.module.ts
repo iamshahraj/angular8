1e1ce0b95b6c492b7221f526bafce8766c568903
Raj Shah <raj16559@gmail.com>
2019-07-26 12:45:05 +0530

    Module 4 done

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { HeaderComponent } from './components/header/header.component';
+
+import { RecipesComponent } from './views/recipes/recipes.component';
+import { RecipeListComponent } from './views/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './views/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './views/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './views/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './views/shopping-list/shopping-edit/shopping-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
